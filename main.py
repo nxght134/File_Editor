270f0459cb6320aae24a7f1dcf3d0ae9ba0b008d
BuildTools <unconfigured@null.spigotmc.org>
2023-06-11 13:45:41 +0300

    Add menu

--- main.py
+++ main.py
@@ -8,11 +8,21 @@ class Application(Frame):
         self.create_menu()
 
     def create_menu(self):
+        Label(self, text="Welcome", font="Arial 50").grid(columnspan=2)
+        Button(self, text="Добавить новую запись в таблицу", font="Arial 20", command=self.add_record).grid(row=1,
+                                                                                                            column=0)
+        Button(self, text="Просмотреть все записи в таблице", font="Arial 20", command=self.show_records).grid(row=1,
+                                                                                                               column=1)
+
+    def add_record(self):
+        pass
+
+    def show_records(self):
         pass
 
 
 root = Tk()
 root.title("File Reader and Editor")
-root.geometry('1000x700')
+root.geometry('920x700')
 app = Application(root)
 root.mainloop()
